d7678ff78c5f1b2662357765c6d1342cbd5b0556
oriolmarch <icewoo2001@gmail.com>
2026-03-12 17:21:02 +0100

    Atributs - Personatge

--- personatge.java
+++ personatge.java
@@ -1,5 +1,28 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class personatge {
-    
+    private String nom;
+    private int edat;
+
+    private int forca;
+    private int destresa;
+    private int constitucio;
+    private int inteligencia;
+    private int saviesa;
+    private int carisma;
+    private String especialitat;
+
+    private int salut;
+    private int salutMax;
+    private int mana;
+    private int manaMax;
+
+    private boolean defensat;
+
+    private ArrayList<arma> armes;
+    private arma armaEquipada;
+
     public personatge(){
 
     }
