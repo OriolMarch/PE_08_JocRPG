bb2d0223e5464d790dcd9a5e94cc2679a35dac1c
oriolmarch <icewoo2001@gmail.com>
2026-03-12 17:09:31 +0100

    Arma

--- arma.java
+++ arma.java
@@ -1,6 +1,49 @@
 public class arma {
     
-    public arma(){
+     private String tipus;
+    private int dany;
+    private boolean magica;
 
-    }   
+    public arma(String tipus, int dany, boolean magica) {
+        this.tipus = tipus;
+        this.dany = dany;
+        this.magica = magica;
+
+        if (this.dany < 1) {
+            this.dany = 1;
+        }
+
+        if (this.dany > 100) {
+            this.dany = 100;
+        }
+    }
+
+     public String getTipus() {
+        return tipus;
+    }
+
+    public int getDany() {
+        return dany;
+    }
+
+    public boolean isMagica() {
+        return magica;
+    }
+
+     @Override
+    public String toString() {
+        String tipusArma;
+
+        if (magica) {
+            tipusArma = "magica";
+        } else {
+            tipusArma = "fisica";
+        }
+
+        return tipus + " | dany: " + dany + " | " + tipusArma;
+    }
 }
+
+
+
+
